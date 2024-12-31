a8bef11d28a2948500d5ba58d4319fec57938078
Stamatis Vogiatzopoulos <stamvogiatzop@gmail.com>
2024-12-31 01:04:33 +0100

    create first round code but with bug

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-console.log("Hello");
+console.log("ROCK___PAPER___SCISSORS___FOR ___NOOBS");
 
 function getComputerChoice() {
   let numberChoice = Math.floor(Math.random() * 3);
@@ -17,4 +17,56 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-getComputerChoice();
+
+// STUPID BUG STARTS WITH HUMAN PROMPT
+
+function getHumanChoice() {
+  debugger;
+
+  // PROBABLY HERE THERE IS THE STUPID BUG...
+
+  let humanChoice = prompt("Choose: ROCK, PAPER or SCISSORS?").toLowerCase();
+
+  debugger;
+
+  // MAYBE ALSO HERE
+
+  return humanChoice;
+}
+
+// let humanScore = 0;
+// let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  switch (humanChoice === "scissors") {
+    case (computerChoice === "rock"):
+      alert("computer...");
+      alert("chose rock! YOU LOSE...");
+      computerScore++;
+      break;
+    case (computerChoice === "scissors"):
+      alert("TIE");
+      break;
+    case (computerChoice === "paper"):
+      alert("computer...");
+      alert("chose paper! YOU WIN!");
+      alert("You\'re a worthy adversary. Beware...")
+      humanScore++
+      break;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playOrNot() {
+  let yesOrNo = prompt("Wanna Play? Press y or n.").toLowerCase();
+
+  if (yesOrNo === "y") {
+    playRound(humanSelection, computerSelection);
+  } else {
+    alert("Bye loser...");
+  }
+}
+
+playOrNot();

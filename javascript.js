3308e6c9f6ef2dc25b8ccc42cc02cd23078d3e50
Stamatis Vogiatzopoulos <stamvogiatzop@gmail.com>
2024-12-31 23:58:46 +0100

    destroyed the whole thing.. start over after line 50.

--- javascript.js
+++ javascript.js
@@ -1,39 +1,10 @@
-console.log("ROCK___PAPER___SCISSORS___FOR ___NOOBS");
+function getHumanChoice() {
 
+  // PROBABLY HERE THERE IS THE STUPID BUG THAT ALWAYS ACTIVATES THE PROMPT...
+  const humanChoice = prompt("Choose: ROCK, PAPER or SCISSORS?").toLowerCase();
 
-// FUNCTION FOR ASKING WHETHER YOU WANT TO PLAY OR NOT!!!//
-
-// function playOrNot() {
-//   let yesOrNo = prompt("Wanna Play? Press y or n").toLowerCase();
-
-//   if (yesOrNo === "y") {
-//     alert("Enter with caution...");
-//   } else {
-//     alert("Bye loser...");
-//   }
-// }
-
-// playOrNot();
-
-function getComputerChoice() {
-  let numberChoice = Math.floor(Math.random() * 3);
-  let computerChoice = "";
-  switch (numberChoice) {
-    case 0:
-      computerChoice = "rock";
-      break;
-    case 1:
-      computerChoice = "paper";
-      break;
-    case 2:
-      computerChoice = "scissors";
-      break;
-  }
-  return computerChoice;
+  return humanChoice;
 }
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
 let humanScore = 0;
 let computerScore = 0;
@@ -57,12 +28,52 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-function getHumanChoice() {
+// const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
-  // PROBABLY HERE THERE IS THE STUPID BUG THAT ALWAYS ACTIVATES THE PROMPT...
+playOrNot();
+
+playRound();
 
 
-  const humanChoice = prompt("Choose: ROCK, PAPER or SCISSORS?").toLowerCase();
 
-  return humanChoice;
+
+
+
+
+
+
+
+
+
+
+console.log("ROCK___PAPER___SCISSORS___FOR ___NOOBS");
+
+// FUNCTION FOR ASKING WHETHER YOU WANT TO PLAY OR NOT!!!//
+
+function playOrNot() {
+  let yesOrNo = prompt("Wanna Play? Press y or n").toLowerCase();
+
+  if (yesOrNo === "y") {
+    alert("Enter with caution...");
+  } else {
+    alert("Bye loser...");
+  }
+}
+
+function getComputerChoice() {
+  let numberChoice = Math.floor(Math.random() * 3);
+  let computerChoice = "";
+  switch (numberChoice) {
+    case 0:
+      computerChoice = "rock";
+      break;
+    case 1:
+      computerChoice = "paper";
+      break;
+    case 2:
+      computerChoice = "scissors";
+      break;
+  }
+  return computerChoice;
 }
